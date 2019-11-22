319d707dcb4b704be51abf2768d29e7ec96c30c7
andrewdolman <andydolman@gmail.com>
2019-11-22 14:46:12 +0100

    update for move to github

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("       package climproxyrecords has moved to GitHub, https://github.com/EarthSystemDiagnostics/climproxyrecords
-        install directly with devtools::install_github(\"EarthSystemDiagnostics/climproxyrecords\")
-        or update your remote.
-
-        # to print the current settings
-        git remote -v
-
-        # to change origin to the new url
-        git remote set-url origin git@github.com:username/repo-name.git")
+  # packageStartupMessage("       package climproxyrecords has moved to GitHub, https://github.com/EarthSystemDiagnostics/climproxyrecords
+  #       install directly with devtools::install_github(\"EarthSystemDiagnostics/climproxyrecords\")
+  #       or update your remote.
+  # 
+  #       # to print the current settings
+  #       git remote -v
+  # 
+  #       # to change origin to the new url
+  #       git remote set-url origin git@github.com:username/repo-name.git")
 }
